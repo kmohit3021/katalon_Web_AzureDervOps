3f241ae6a2175a6e89db5c954da488c00afb77e6
katalon <mohit.kumar@katalon.com>
2021-06-09 22:36:31 +0700

    Revert "Added - New Salesforce test"
    This reverts commit e4c18ba03f28817f08c5af49182490b196809d25.

--- Test Suites/Regression Suite/Test Suite Collection/TS Collection for parallel run.ts
+++ Test Suites/Regression Suite/Test Suite Collection/TS Collection for parallel run.ts
@@ -3,18 +3,9 @@
    <description></description>
    <name>TS Collection for parallel run</name>
    <tag></tag>
-   <executionMode>SEQUENTIAL</executionMode>
+   <executionMode>PARALLEL</executionMode>
    <maxConcurrentInstances>2</maxConcurrentInstances>
    <testSuiteRunConfigurations>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Regression Suite/Login into salesforce application</testSuiteEntity>
-      </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
